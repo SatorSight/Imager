41e663c44dfab0d2b1d87629e73c70c9818c3c8d
satorsight <satorsight@gmail.com>
2017-11-08 17:44:31 +0300

    visual fixes

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -3,4 +3,12 @@ module ImagesHelper
 		#Rails.root.to_s+'/public'+image.image_file.url(:original, timestamp: false)
 		image.image_file.url(:original, timestamp: false)
 	end
+
+	def image_width(image)
+		i = image.image_file
+		geometry = Paperclip::Geometry.from_file(i)
+		width = geometry.width.to_s << 'px'
+		width.slice! '.0'
+		width
+	end
 end
